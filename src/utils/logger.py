d7839410493966c0d10d52e2571ba9b1547147aa
EnchantedPy <infinite.mendum@gmail.com>
2025-06-05 10:57:54 +0300

    ..

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,40 +1,16 @@
+from config.settings import BASE_DIR
+from yaml import safe_load
+from logging.config import dictConfig
 import logging
-from pathlib import Path
-from config.settings import Settings, TestSettings
 
-LOG_FORMAT = (
-    "%(asctime)s\t%(name)s\t%(levelname)s\t"
-    "%(filename)s:%(lineno)d\t%(message)s"
-)
-DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-
-formatter = logging.Formatter(LOG_FORMAT, datefmt=DATE_FORMAT)
-
-def setup_logger(name: str, log_file: str, level=logging.INFO, stream: bool = True):
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    logger.propagate = False
-
-    file_handler = logging.FileHandler(log_file, mode="a", encoding="utf-8")
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-    if stream:
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter)
-        logger.addHandler(stream_handler)
-
-    return logger
+config_file_path = BASE_DIR / 'logging.yaml'
 
 
-logger = setup_logger(
-    name="Logger",
-    log_file=Path("logs/app.log"),
-    level=getattr(logging, Settings.log_level.upper(), logging.INFO)
-)
+with open(config_file_path, 'r', encoding='utf-8') as f:
+	log_config_dict = safe_load(f)
+   
+    
+dictConfig(log_config_dict)
 
-test_logger = setup_logger(
-    name="tests",
-    log_file=Path("logs/tests.log"),
-    level=getattr(logging, TestSettings.log_level.upper(), logging.DEBUG)
-)
+logger = logging.getLogger("logger")
+test_logger = logging.getLogger("test_logger")
