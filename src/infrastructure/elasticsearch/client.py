57779b2829961b709ec6cc0c60f1070b99035c9d
EnchantedPy <infinite.mendum@gmail.com>
2025-06-07 22:08:50 +0300

    i hate message brokers

--- src/infrastructure/elasticsearch/client.py
+++ src/infrastructure/elasticsearch/client.py
@@ -6,23 +6,24 @@ from elasticsearch import AsyncElasticsearch
 from config.settings import Settings
 
 
-es = AsyncElasticsearch(hosts=[Settings.elastic_url], basic_auth=(Settings.elastic_user, Settings.elastic_password), verify_certs=False, headers={
-            "Accept": "application/vnd.elasticsearch+json; compatible-with=8",
-            "Content-Type": "application/vnd.elasticsearch+json; compatible-with=8"
-        })
+# es = AsyncElasticsearch(hosts=[Settings.elastic_url], basic_auth=(Settings.elastic_user, Settings.elastic_password), verify_certs=False, headers={
+#             "Accept": "application/vnd.elasticsearch+json; compatible-with=8",
+#             "Content-Type": "application/vnd.elasticsearch+json; compatible-with=8"
+#         })
 
-class EsClientFactory:
-	def __init__(self, hosts=[Settings.elastic_url], basic_auth=(Settings.elastic_user, Settings.elastic_password), verify_certs=False, headers={
-            "Accept": "application/vnd.elasticsearch+json; compatible-with=8",
-            "Content-Type": "application/vnd.elasticsearch+json; compatible-with=8"
-        }):
-		self.client = AsyncElasticsearch(hosts, basic_auth, verify_certs, headers)
 
-	@asynccontextmanager
-	async def get_es_client(self) -> AsyncGenerator[AsyncElasticsearch, None]:
-		yield es
+class ElasticClient:
+    def __init__(self):
+        self.client = AsyncElasticsearch(
+            hosts=[Settings.elastic_url],
+            basic_auth=(Settings.elastic_user, Settings.elastic_password),
+            verify_certs=False,
+				headers={
+                    "Accept": "application/vnd.elasticsearch+json; compatible-with=8",
+						  "Content-Type": "application/vnd.elasticsearch+json; compatible-with=8"
+            })
 
-	async def __call__(self) -> AsyncElasticsearch:
-		return self.__call__()
+    async def __call__(self) -> AsyncElasticsearch:
+        return self.client
 	
-es_clientfactory = EsClientFactory()
+elastic_client = ElasticClient()
--- workers/app.py
+++ workers/app.py
@@ -2,4 +2,4 @@ from celery import Celery
 from config.settings import Settings
 
 
-app = Celery(name='worker', broker=Settings.rmq_url)
+app = Celery(name='worker', broker=Settings.redis_url)
